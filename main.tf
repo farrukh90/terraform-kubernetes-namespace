fa5f3a01c5cd8482daa62fa3774d7ce7df8de56b
farrukh90 <farrukhsadykov@gmail.com>
2023-11-22 18:41:27 -0600

    added size

--- main.tf
+++ main.tf
@@ -5,3 +5,15 @@ resource "kubernetes_namespace" "example" {
     name        = var.name
   }
 }
+
+resource "kubernetes_resource_quota" "pod-limit" {
+  metadata {
+    name = "pod-limit"
+  }
+  spec {
+    hard = {
+      pods = 1000
+    }
+    scopes = ["BestEffort"]
+  }
+}
